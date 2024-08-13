06468a04d55ef8500eda62665423c4ff2cf42477
KHACHCHAB Mohamed (SNCF RESEAU / Directions Techniques Réseau / DGNum META SWD1) <0202933E@commun.ad.sncf.fr>
2024-08-13 16:34:14 +0200

    api service

---
+++ FO-pets-garden/lib/services/api_service.dart
@@ -0,0 +1,48 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class ApiService {
+  static const String baseUrl = "http://localhost/BO%20pets%20garden/";
+
+  Future<List<dynamic>> getAds() async {
+    final response = await http.get(Uri.parse(baseUrl + "get_ads.php"));
+    if (response.statusCode == 200) {
+      return json.decode(response.body);
+    } else {
+      throw Exception('Failed to load ads');
+    }
+  }
+
+  Future<void> addAd(Map<String, dynamic> adData) async {
+    final response = await http.post(
+      Uri.parse(baseUrl + "add_ad.php"),
+      headers: {"Content-Type": "application/json"},
+      body: json.encode(adData),
+    );
+    if (response.statusCode != 200) {
+      throw Exception('Failed to add ad');
+    }
+  }
+
+  Future<void> updateAd(Map<String, dynamic> adData) async {
+    final response = await http.post(
+      Uri.parse(baseUrl + "update_ad.php"),
+      headers: {"Content-Type": "application/json"},
+      body: json.encode(adData),
+    );
+    if (response.statusCode != 200) {
+      throw Exception('Failed to update ad');
+    }
+  }
+
+  Future<void> deleteAd(int id) async {
+    final response = await http.post(
+      Uri.parse(baseUrl + "delete_ad.php"),
+      headers: {"Content-Type": "application/json"},
+      body: json.encode({"id": id}),
+    );
+    if (response.statusCode != 200) {
+      throw Exception('Failed to delete ad');
+    }
+  }
+}
